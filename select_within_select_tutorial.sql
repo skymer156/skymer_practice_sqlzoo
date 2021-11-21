bc0ec298c6050864531fb9ae25be5f1c0bc4690c
skymer156 <46322826+skymer156@users.noreply.github.com>
2021-11-21 16:07:36 +0900

    幾つかのsql問題を回答。 select_within_select_tutortial.sqlはまだ途中。

---
+++ select_within_select_tutorial.sql
@@ -0,0 +1,17 @@
+SELECT name FROM world WHERE population > (SELECT population FROM world WHERE name='Russia')
+
+select name from world where (gdp/population) > (select gdp/population from world where name = 'United Kingdom') and continent = 'Europe';
+
+select name, continent from world where continent in ( select continent from world where name in ('Argentina', 'Australia') ) order by name ;
+
+select name, population from world where population > ( select population from world where name = 'Canada' )  and population < ( select population from world where name = 'Poland' );
+
+select name, concat(round(population/(select population from world where name = 'Germany') * 100, 0), '%') from world where continent = 'Europe';
+
+select name from world where gdp > ALL( select gdp from world where continent = 'Europe' and gdp > 0 );
+
+SELECT continent, name, area FROM world x WHERE area >= ALL (SELECT area FROM world y WHERE y.continent=x.continent);
+
+select continent, name from world x where name <= all( select name from world y where x.continent = y.continent);
+
+
